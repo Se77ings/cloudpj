ed0b4315ed14311b1d6f51bd7a1a9ccdd7ca1eb3
unknown <se77ings@gmail.com>
2023-08-21 10:22:36 -0300

    update

---
+++ Financeiro/Pagamentos/Pagamentos.php
@@ -0,0 +1,22 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if(isset($_POST['selectCategory'])){
+        $conn = mysqli_connect("localhost", "root", "", "financialDB");
+        $sql = "SELECT * FROM categoria";
+        $result = mysqli_query($conn, $sql);
+        $categories = array();
+
+        while($row = mysqli_fetch_assoc($result)){
+            $categories[] = $row;
+        }
+
+        echo json_encode($categories);
+        mysqli_close($conn);
+
+        exit;
+
+        
+    }
+}
+
+?>
